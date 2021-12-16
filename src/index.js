8738facd934e4225f07dc3ef3a8e7d1d7e25bf52
v_jiawali(李嘉旺) <v_jiawali@tencent.com>
2021-12-16 17:15:02 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Create from './pages/create/index'
+ReactDOM.render(
+  <React.StrictMode>
+    <Create />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/pages/create/index.jsx
@@ -0,0 +1,12 @@
+import react, { Component } from "react";
+class Create extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div>
+            111
+        </div>
+    }
+}
+export default Create;
